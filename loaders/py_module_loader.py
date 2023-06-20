7fed06c9ebf08b3b54c8b2730a13f33f5db2055c
Dale Appleby <dale@farpoint.co.nz>
2023-06-21 10:02:12 +1200

    changes

--- loaders/py_module_loader.py
+++ loaders/py_module_loader.py
@@ -1,25 +1,23 @@
 """
     ModuleLoader
 """
-from types import ModuleType
-import importlib.util
-from . import Loader
 import os
-
-
-class PyScriptLoader(importlib.machinery.SourceFileLoader):
-    def __init__(self, module_name, module_path):
-        super().__init__(module_name, module_path)
-
+import importlib.util
+from types import ModuleType
+from importlib.machinery import SourceFileLoader
+from . import Loader
 
 class PyModuleLoader(Loader):
     """
         ModuleLoader
     """
     async def _load(self, script_path: str, module_name: str) -> ModuleType:
+        if not script_path.endswith('.py'):
+            raise ValueError("Invalid script path, must end with .py")
+        
         spec = importlib.util.spec_from_file_location(
             module_name,
-            loader=PyScriptLoader(module_name, script_path),
+            loader=SourceFileLoader(module_name, script_path),
             location=script_path
         )
         module = importlib.util.module_from_spec(spec)
