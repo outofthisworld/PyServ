2d2668217dd61bf00679e883250a548db86e6003
Dale <dale@farpoint.co.nz>
2023-06-22 07:30:50 +0000

    changes

--- config/app.py
+++ config/app.py
@@ -7,4 +7,4 @@ from .parser import parser
 
 ROOT_DIR = path.join(path.dirname(path.abspath(__file__)))
 PLUGINS_DIR = path.join(ROOT_DIR, parser.get(
-    'app', 'plugin_folder', fallback='plugins'))
+    'app', 'plugins_folder', fallback='plugins'))
