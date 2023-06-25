22f4f74fbb2dc31c27ef39d97d6237372c12ffc7
Dale Appleby <dale@farpoint.co.nz>
2023-06-25 19:56:07 +1200

    changes

--- world/world_task.py
+++ world/world_task.py
@@ -1,6 +1,7 @@
 import typing
 from typing import Optional
 
+
 class WorldTask:
     def __init__(self):
         self._task = None
@@ -9,69 +10,65 @@ class WorldTask:
     def fromCallable(cls, task: typing.Callable) -> 'WorldTask':
         if not callable(task):
             raise ValueError("Must be a callable task")
-        
+
         task = cls()
         task._task = task
         return task
 
     def __call__(self) -> None:
         self.poll()
-    
+
     def poll(self) -> None:
         if callable(self._task):
             self._task()
-            
+
     def done(self) -> bool:
         return False
 
     @property
     def task(self, task: typing.Callable) -> None:
         self.fromCallable(task)
-        
+
+
 class IntervalWorldTask(WorldTask):
-    def __init__(self, delay:int = 0, initialDelay=False, times:Optional[int] = None):
+    def __init__(self, delay: int = 0, initialDelay=False, times: Optional[int] = None):
         super().__init__()
         self._delay = max(delay, 0)
         self._times = times
         self._attempts = 0 if not initialDelay else self._delay
-        
+
     def __str__(self):
         return f"IntervalWorldTask(delay={self._delay},times={self._times},attempts={self._attempts})"
-    
+
     @classmethod
     def fromCallable(cls, task: typing.Callable, **kwargs) -> 'IntervalWorldTask':
-        interval_task = IntervalWorldTask()
-        
+        interval_task = IntervalWorldTask(kwargs.get('delay', 0), kwargs.get(
+            'initialDelay', False), kwargs.get('times'))
+
         if not callable(task):
             raise ValueError("Invalid task, must be callable")
-        
+
         # Set the task
         interval_task._task = task
-        # Set additional attributes specific to IntervalWorldTask
-        interval_task._delay = kwargs.get('delay', 0)
-        interval_task._times = kwargs.get('times')
-        interval_task._attempts = 0 if kwargs.get('initialDelay', False) else interval_task._delay
 
         return interval_task
 
-    def poll(self) -> None: 
+    def poll(self) -> None:
         if self._times is not None and self._times <= 0:
             return
-        
+
         if self._attempts > 0:
             self._attempts = self._attempts - 1
             return
-            
+
         super().poll()
         self._attempts = self._delay
-        
+
         if self._times is not None:
             self._times = self._times - 1
-            
+
     def done(self) -> bool:
         if self._times is not None and self._times <= 0:
             return True
-        
+
         return False
-        
-        
